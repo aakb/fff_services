088929dbf502cd79c0a7365fe927c783119390ab
Jesper Kristensen <cableman@linuxdev.dk>
2012-03-05 16:21:54 +0100

    Started on the fact web-service

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+include_once dirname(__FILE__).'/../utils/utils.inc';
+include_once dirname(__FILE__).'/../database/fact.inc';
+
+define('FFF_GET_FACT', 'getFact');
+define('FFF_GET_GUID', 'getGuid');
+
+function parseOptions() {
+  $options = array();
+  try {
+    $options['callback'] = Utils::getCallback();
+    $options['method'] = Utils::getMethod();
+  } catch (Exception $e) {
+    echo $e->getMessage();
+    exit($e->getCode());
+  }
+
+  // If getFact id should be present as well.
+  if ($options['method'] == FFF_GET_FACT) {
+   try {
+      $options['id'] = Utils::getGUID();
+    } catch (Exception $e) {
+      echo $e->getMessage();
+      exit($e->getCode());
+    }
+  }
+
+  return $options;
+}
+
+function returnJson($result) {
+   header('Content-type: application/json');
+   echo json_encode($result);
+}
+
+$options = parseOptions();
+switch ($options['method']) {
+  case 'getFact':
+    
+    break;
+
+  case 'getNumber':
+    
+    break;
+
+  default:
+    echo 'Method is not available...';
+    exit(-1);
+    break;
+}
+
